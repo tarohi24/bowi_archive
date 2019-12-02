af2196c8c802498d86e3def6bf09270cb443decb
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-12-02 05:50:01 +0000

    Be sure to pass clef_converter's tests
    - Add test materials

--- bowi/tests/initialize/converters/clef_test.py
+++ bowi/tests/initialize/converters/clef_test.py
@@ -6,12 +6,30 @@ import xml.etree.ElementTree as ET
 import pytest
 
 from bowi.initialize.converters.clef import CLEFConverter, NoneException
-from bowi.settings import data_dir
+from bowi import settings
 
 
-class CLEFConverterTest(unittest.TestCase):
+@pytest.fixture(autouse=True)
+def patch_data_dir(monkeypatch):
+    monkeypatch.setattr(settings, 'data_dir',
+                        settings.project_root.joinpath('bowi/tests/data'))
 
-    def to_xml_root(self, docid: str) -> ET.Element:
+
+@pytest.fixture
+def converter() -> CLEFConverter:
+    return CLEFConverter()
+
+
+@pytest.fixture
+def docids() -> List[str]:
+    docids: List[str] = ['EP-0050001-A2', 'EP-1010180-B1']
+    return docids
+
+
+@pytest.fixture
+def roots(docids) -> List[ET.Element]:
+
+    def to_xml_root(docid: str) -> ET.Element:
         """
         Parameters
         ----------
@@ -23,52 +41,34 @@ class CLEFConverterTest(unittest.TestCase):
         second: str = docid[4:6]
         third: str = docid[6:8]
         forth: str = docid[8:10]
-
-        fpath: Path = data_dir.joinpath(
-            f'clef/orig/collection/00000{first}/{second}/{third}/{forth}/{docid}.xml')
-
+        fpath: Path = settings.data_dir.joinpath(
+            f'clef/orig/collection/00000{first}/{second}/{third}/{forth}/{docid}.xml')  # noqa
         root: ET.Element = ET.parse(str(fpath.resolve())).getroot()
         return root
 
-    def __init__(self, *args, **kwargs):
-        super(CLEFConverterTest, self).__init__(*args, **kwargs)
-        self.converter: CLEFConverter = CLEFConverter()
-        self.docids: List[str] = [
-            'EP-0050001-A2',
-            'EP-1010180-B1'
-        ]
-        self.roots: List[ET.Element] = [self.to_xml_root(docid) for docid in self.docids]
+    roots: List[ET.Element] = [to_xml_root(docid) for docid in docids]
+    return roots
 
-    def test_get_title(self):
-        titles: List[str] = [
-            'A golf aid.',
-            'A READ-ONLY MEMORY AND READ-ONLY MEMORY DEVICE',
-        ]
-        self.assertListEqual(
-            titles,
-            [self.converter._get_title(root) for root in self.roots])
 
-    def test_get_docid(self):
-        self.assertListEqual(
-            [docid.replace('-', '') for docid in self.docids],
-            [self.converter._get_docid(root) for root in self.roots])
+def test_get_title(converter, roots):
+    titles: List[str] = [
+        'A golf aid.',
+        'A READ-ONLY MEMORY AND READ-ONLY MEMORY DEVICE',
+    ]
+    assert titles == [converter._get_title(root) for root in roots]
 
-    def test_get_tags(self):
-        self.assertListEqual(
-            [set('A63B'.split()),
-             set('H01L G11C'.split())],
-            [set(self.converter._get_tags(root)) for root in self.roots])
+def test_get_docid(converter, roots, docids):
+    assert [docid.replace('-', '') for docid in docids]\
+        == [converter._get_docid(root) for root in roots]
 
-    def test_get_text(self):
-        with pytest.raises(NoneException):
-            self.converter._get_text(self.roots[0])
+def test_get_tags(converter, roots):
+    assert set(converter._get_tags(roots[0])) == {'A63B'}
+    assert set(converter._get_tags(roots[1])) == {'H01L', 'G11C'}
 
-        self.assertListEqual(
-            'The present invention'.split(),
-            self.converter._get_text(self.roots[1]).split()[:3])
 
-    def test_get_paras(self):
-        with pytest.raises(NoneException):
-            self.converter._get_paragraph_list(self.roots[0])
+def test_get_text(converter, roots):
+    with pytest.raises(NoneException):
+        converter._get_text(roots[0])
 
-        assert len(self.converter._get_paragraph_list(self.roots[1])) == 40
+    assert 'The present invention'.split()\
+        == converter._get_text(roots[1]).split()[:3]
