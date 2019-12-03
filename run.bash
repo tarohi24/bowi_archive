95bb29e54ecb5bb9e0e79ef7881c25e3071f9229
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-12-03 07:05:12 +0000

    Add trec option in run.bash

--- run.bash
+++ run.bash
@@ -11,6 +11,12 @@ case $1 in
     "python" )
         docker-compose run --rm python bash scripts/run_scripts.bash ${@:2}
         ;;
+    "trec" )
+        PREC_FILE=$2
+        DATASET=(${PREC_FILE//\// })
+        DATASET=${DATASET[1]}
+        docker-compose -f compose/trec/docker-compose.yaml run --rm trec trec_eval -m recall -q results/${DATASET}/gt.qrel $PREC_FILE
+        ;;
     * )
         echo "Invalid option ${1}" ;;
 esac
