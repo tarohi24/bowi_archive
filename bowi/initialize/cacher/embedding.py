0a83eecc5a5c73e164860be4f2931e70e1a46756
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-12-07 04:39:27 +0000

    Implement embedding cacher along typedflow format

--- bowi/initialize/cacher/embedding.py
+++ bowi/initialize/cacher/embedding.py
@@ -1,54 +1,80 @@
 """
-Save Embeddings of queries
+Save Embeddings of queries with the word
 """
-import argparse
+from __future__ import annotations
+from dataclasses import dataclass, field
 from pathlib import Path
-from typing import List
+from typing import List, Optional, ClassVar, Type, Iterable, Generator
 
 import numpy as np
-from tqdm import tqdm
+from typedflow.flow import Flow
+from typedflow.nodes import DumpNode, TaskNode
 
 from bowi import settings
 from bowi.embedding.fasttext import FastText
+from bowi.methods.common.types import Param
+from bowi.methods.common.methods import Method
 from bowi.models import Document
 from bowi.utils.text import get_all_tokens
 
 
-ft_model: FastText = FastText()
+@dataclass
+class EmbeddingCacheParam(Param):
+    embed_model: str
+
+    @classmethod
+    def from_args(cls, args) -> Param:
+        return EmbeddingCacheParam(embed_model=args.embed_model)
 
 
-def load_bulk(dataset: str) -> List[Document]:
-    dump_path: Path = settings.data_dir.joinpath(f'{dataset}/query/dump.bulk')
-    with open(dump_path) as fin:
-        docs: List[Document] = [Document.from_json(line)  # type: ignore
-                                for line in fin.read().splitlines()]
-    return docs
+@dataclass
+class EmbededWord:
+    word: str
+    vec: np.ndarray
 
 
-def dump(doc: Document, dataset: str) -> None:
-    dump_path: Path = settings.cache_dir.joinpath(
-        f'{dataset}/embedding/{doc.docid}.bulk')
-    # tokenize
-    tokens: List[str] = get_all_tokens(doc.text)
-    # embed
-    ary: np.ndarray = np.array([
-        vec for vec in ft_model.embed_words(tokens)
-        if vec is not None])
-    np.save(dump_path, ary)
+@dataclass
+class EmbeddingCacher(Method[EmbeddingCacheParam]):
+    param_type: ClassVar[Type] = EmbeddingCacheParam
+    fasttext: FastText = field(init=False)  # TODO: models should not be fixed
 
+    def __post_init__(self):
+        self.fasttext: FastText = FastText()
 
-def main() -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('dataset',
-                        metavar='D',
-                        type=str,
-                        nargs=1)
-    args = parser.parse_args()
-    dataset: str = args.dataset[0]
-    for doc in tqdm(load_bulk(dataset=dataset)):
-        dump(doc, dataset=dataset)
-    return 0
+    def embed(self,
+              doc: Document) -> Generator[EmbededWord, None, None]:
+        tokens: List[str] = get_all_tokens(doc.text)
+        emb_list: List[Optional[np.ndarray]] = self.ft_model.embed(tokens)
+        for tok, vec in zip(tokens, emb_list):
+            if vec is not None:
+                yield EmbededWord(word=tok, vec=vec)
+        return
 
+    def dump(self,
+             ews: Iterable[EmbededWord],
+             doc: Document) -> None:
+        """
+        Dump both keywords and their embeddings
 
-if __name__ == '__main__':
-    exit(main())
+        - EPXXXXX (dir)
+        |- words.txt
+        |- embeddings.npy
+        """
+        dump_dir: Path = settings.cache_dir.joinpath(
+            f'{self.context.dataset}/embedding/{doc.docid}')
+        emb_list: List[np.ndarray] = []
+        with open(dump_dir.joinpath('words.txt'), 'w') as fout:
+            for ew in ews:
+                fout.write(f'{ew.word}\n')
+                emb_list.append(ew.vec)
+        embs: np.ndarray = np.array(emb_list)
+        np.save(dump_dir.joinpath('embeddings.npy'), embs)
+
+    def create_flow(self) -> Flow:
+        node_embed: TaskNode = TaskNode(func=self.embed)
+        (node_embed < self.load_node)('doc')
+        node_dump: DumpNode = DumpNode(func=self.dump)
+        (node_dump < node_embed)('ews')
+        (node_dump < self.load_node)('doc')
+        flow: Flow = Flow(dump_nodes=[node_dump, ])
+        return flow
