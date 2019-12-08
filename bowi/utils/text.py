95165ab9c66a27694ae2dc27b6f6eadf5d718a25
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-12-08 08:31:42 +0000

    Use Elastiscearch analyzer to stem queries
    - Experimental results show using analyzer affect somehow on
      the keywords quality

--- bowi/utils/text.py
+++ bowi/utils/text.py
@@ -1,12 +1,12 @@
 import re
-from typing import List, Pattern, Set
+from typing import Dict, List, Pattern, Set
 
 from nltk.corpus import stopwords as nltk_sw
-from nltk.tokenize import RegexpTokenizer
+
+from bowi.settings import es
 
 
 stopwords: Set[str] = set(nltk_sw.words('english'))
-tokenizer: RegexpTokenizer = RegexpTokenizer(r'\w+|\$[\d\.]+|\S+')
 not_a_word_pat: Pattern = re.compile(r'^[^a-z0-9]*$')
 
 
@@ -14,12 +14,18 @@ def get_all_tokens(text: str) -> List[str]:
     """
     Preprocessing + tokenize
     """
-    tokens: List[str] = tokenizer.tokenize(text.lower())
-    # remove stopwords
+    tokens: List[str] = stem_words(text)
+    # While stem_words removes some stopwords, nltk stopword list is richer
     tokens: List[str] = [w for w in tokens if w not in stopwords]  # type: ignore
     tokens: List[str] = [w for w in tokens  # type: ignore
-                         if not_a_word_pat.match(w) is None
-                         and not w.isdigit()]
+                         if not_a_word_pat.match(w) is None and not w.isdigit()]
     tokens: List[str] = [w.replace('(', '').replace(')', '').replace('-', '')  # type: ignore
                          for w in tokens]
+    return tokens
+
+
+def stem_words(text: str) -> List[str]:
+    body: Dict = {"analyzer": "english", "text": text}
+    res: Dict = es.indices.analyze(body=body)
+    tokens: List[str] = [tok['token'] for tok in res['tokens']]
     return tokens
