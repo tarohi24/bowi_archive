c010bd6b7db600116eb37de34d2272ad041f2ba3
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-12-13 07:51:02 +0000

    Improve efficiency of fuzzy.rerank
    - Remove duplicated computation
      - Introduce TFIDF
    - New style typedflow
    - Add debug mode to the creation of flow

--- bowi/methods/methods/fuzzy/rerank.py
+++ bowi/methods/methods/fuzzy/rerank.py
@@ -3,16 +3,16 @@ Note: This module assserts keywords are generated
 in advance. Run fuzzy.naive in the same params
 before running this script
 """
-from collections import Counter
 from dataclasses import dataclass, field
 import json
 import logging
 from pathlib import Path
-from typing import ClassVar, Dict, List, Type, Optional
+from typing import ClassVar, Dict, List, Type, Tuple
 
 import numpy as np
 from typedflow.flow import Flow
 from typedflow.nodes import TaskNode, LoaderNode
+from tqdm import tqdm
 
 from bowi.embedding.base import mat_normalize
 from bowi.embedding.fasttext import FastText
@@ -35,15 +35,21 @@ class QueryKeywords:
 
 
 @dataclass
+class TfidfEmb:
+    words: List[str]
+    tfs: np.ndarray  # int
+    idfs: np.ndarray  # float
+    embs: np.ndarray  # (n_words, n_dim)
+
+
+@dataclass
 class FuzzyRerank(Method[FuzzyParam]):
     param_type: ClassVar[Type] = FuzzyParam
     fasttext: FastText = field(init=False)
-    es_client: EsClient = field(init=False)
 
     def __post_init__(self):
         super(FuzzyRerank, self).__post_init__()
         self.fasttext: FastText = FastText()
-        self.es_client: EsClient = EsClient(es_index=self.context.es_index)
 
     def load_keywords(self) -> List[QueryKeywords]:
         path: Path = settings.cache_dir.joinpath(
@@ -54,28 +60,44 @@ class FuzzyRerank(Method[FuzzyParam]):
                                     for key, val in data.items()]
         return lst
 
-    def load_query_mat(self,
-                       qk: QueryKeywords) -> np.ndarray:
-        docid: str = qk.docid
-        mat: np.ndarray = np.load(settings.cache_dir.joinpath(
-            f'{self.context.es_index}/embeddings/{docid}/embeddings.npy'))
-        return mat
+    def _get_tfidf_emb(self,
+                       docid: str,
+                       es_index: str) -> TfidfEmb:
+        previous_level = logging.root.manager.disable
+        logging.disable(logging.WARN)  # supress logger of Elasticsearch
+        es_client: EsClient = EsClient(es_index=es_index)
+        tfidf_dict: Dict[str, Tuple[int, float]] = {
+            word: tfidf
+            for word, tfidf in es_client.get_tfidfs(docid=docid).items()
+            if self.fasttext.isin_vocab(word) and tfidf[0] >= self.param.min_tf
+        }
+        words, tfidfs = list(zip(*tfidf_dict.items()))
+        tfs, idfs = [np.array(lst) for lst in list(zip(*tfidfs))]
+        embs: np.ndarray = self.fasttext.embed_words(words)
+        logging.disable(previous_level)
+        return TfidfEmb(words=words, tfs=tfs, idfs=idfs, embs=embs)
 
-    def get_col_embs(self,
-                     qk: QueryKeywords) -> Dict[str, np.ndarray]:
+    def load_query(self,
+                   qk: QueryKeywords) -> TfidfEmb:
+        docid: str = qk.docid
+        tfidf_emb: TfidfEmb = self._get_tfidf_emb(docid=docid,
+                                                  es_index=f'{self.context.es_index}_query')
+        return tfidf_emb
+
+    def get_col_tfidfs(self,
+                       qk: QueryKeywords) -> Dict[str, TfidfEmb]:
         """
         Get documents cached by keywrod search
         """
-        embs: Dict[str, np.ndarray] = {}
-        for doc in load_cols(docid=qk.docid,
-                             runname='100',
-                             dataset=self.context.es_index):
-            tokens: List[str] = self.es_client.get_tokens_from_doc(
-                docid=doc.docid)
-            vecs: List[Optional[np.ndarray]] = self.fasttext.embed_words(tokens)
-            embs[doc.docid] = np.array(
-                [vec for vec in vecs if vec is not None])
-        return embs
+        tfidf_embs: Dict[str, TfidfEmb] = dict()
+
+        for doc in tqdm(load_cols(docid=qk.docid,
+                                  runname='100',
+                                  dataset=self.context.es_index),
+                        desc='Getting colembs...'):
+            tfidf_embs[doc.docid] = self._get_tfidf_emb(docid=doc.docid,
+                                                        es_index=self.context.es_index)
+        return tfidf_embs
 
     def get_kembs(self,
                   qk: QueryKeywords) -> np.ndarray:
@@ -84,14 +106,10 @@ class FuzzyRerank(Method[FuzzyParam]):
 
         Parameter
         -----
-        doc_num
+        docFalse
             Document number (starts with 0)
         """
-        emb_list: List[np.ndarray] = [
-            vec for vec in self.fasttext.embed_words(qk.keywords)
-            if vec is not None]
-        embs: np.ndarray = np.array(emb_list)
-        print(embs.shape)
+        embs: np.ndarray = np.array(self.fasttext.embed_words(qk.keywords))
         return mat_normalize(embs)
 
     def _get_nns(self,
@@ -109,7 +127,7 @@ class FuzzyRerank(Method[FuzzyParam]):
         return nns
 
     def to_fuzzy_bows(self,
-                      mat: np.ndarray,
+                      tfidf_emb: TfidfEmb,
                       keyword_embs: np.ndarray) -> np.ndarray:
         """
         Generate a FuzzyBoW vector according to keyword_embs
@@ -118,18 +136,21 @@ class FuzzyRerank(Method[FuzzyParam]):
         -----
         1D array whose item[i] is the normalized frequency of ith keyword
         """
-        nns = self._get_nns(mat=mat, keyword_embs=keyword_embs)
-        counter: Counter = Counter(nns)
-        counts: List[int] = [counter[i] if i in counter else 0
-                             for i in range(keyword_embs.shape[0])]
-        return np.array(counts) / np.sum(counts)
+        mat: np.ndarray = tfidf_emb.embs
+        tfidfs: np.ndarray = tfidf_emb.tfs * tfidf_emb.idfs
+        assert tfidfs.ndim == 1
+        nns: List[int] = self._get_nns(mat=mat, keyword_embs=keyword_embs)
+        scores: np.ndarray = np.array([0.0 for _ in range(len(keyword_embs))])
+        for i in range(len(nns)):
+            scores[nns[i]] = tfidfs[i]
+        return scores / np.sum(scores)  # normalize
 
     def get_collection_fuzzy_bows(self,
-                                  col_embs: Dict[str, np.ndarray],
+                                  col_items: Dict[str, TfidfEmb],
                                   keyword_embs: np.ndarray) -> Dict[str, np.ndarray]:
         bow_dict: Dict[str, np.ndarray] = {
-            docid: self.to_fuzzy_bows(mat=emb, keyword_embs=keyword_embs)
-            for docid, emb in col_embs.items()
+            docid: self.to_fuzzy_bows(tfidf_emb=item, keyword_embs=keyword_embs)
+            for docid, item in col_items.items()
         }
         return bow_dict
 
@@ -147,33 +168,34 @@ class FuzzyRerank(Method[FuzzyParam]):
         return TRECResult(query_docid=qk.docid,
                           scores=scores)
 
-    def create_flow(self):
+    def create_flow(self, debug: bool = False):
         # loading query
         loader: LoaderNode = LoaderNode(func=self.load_keywords)
-        n_get_col_embs = TaskNode(self.get_col_embs)
-        (n_get_col_embs < loader)('qk')
-        n_get_kembs = TaskNode(self.get_kembs)
-        (n_get_kembs < loader)('qk')
+        n_get_cols = TaskNode(self.get_col_tfidfs)({'qk': loader})
+        n_get_kembs = TaskNode(self.get_kembs)({'qk': loader})
 
         # generate fBoW of the query
-        n_load_query_mat = TaskNode(self.load_query_mat)
-        (n_load_query_mat < loader)('qk')
-        n_fbow = TaskNode(self.to_fuzzy_bows)
-        (n_fbow < n_load_query_mat)('mat')
-        (n_fbow < n_get_kembs)('keyword_embs')
+        n_load_query = TaskNode(self.load_query)({'qk': loader})
+        n_fbow = TaskNode(self.to_fuzzy_bows)({
+            'tfidf_emb': n_load_query,
+            'keyword_embs': n_get_kembs
+        })
 
         # generate fBoW of collection
-        n_cfbow = TaskNode(self.get_collection_fuzzy_bows)
-        (n_cfbow < n_get_col_embs)('col_embs')
-        (n_cfbow < n_get_kembs)('keyword_embs')
+        n_cfbow = TaskNode(self.get_collection_fuzzy_bows)({
+            'col_items': n_get_cols,
+            'keyword_embs': n_get_kembs
+        })
 
         # integration
-        n_match = TaskNode(self.match)
-        (n_match < loader)('qk')
-        (n_match < n_fbow)('query_bow')
-        (n_match < n_cfbow)('col_bows')
+        n_match = TaskNode(self.match)({
+            'qk': loader,
+            'query_bow': n_fbow,
+            'col_bows': n_cfbow
+        })
 
         (self.dump_node < n_match)('res')
-        flow: Flow = Flow(dump_nodes=[self.dump_node, ])
+        flow: Flow = Flow(dump_nodes=[self.dump_node, ],
+                          debug=debug)
         flow.typecheck()
         return flow
