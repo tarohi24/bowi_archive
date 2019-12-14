85066449483046f682f840ef11a825bcb876920f
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-12-14 11:55:55 +0000

    Implement CMU converter
    - implement to_document
    - integrated tests

--- bowi/initialize/converters/cmu.py
+++ bowi/initialize/converters/cmu.py
@@ -2,17 +2,9 @@ from dataclasses import dataclass, field
 import logging
 from pathlib import Path
 import re
-import sys
 from typing import List, Optional, Pattern, Match, Tuple
-import xml.etree.ElementTree as ET
 
-from bowi.initialize.converters.base import (
-    Converter,
-    CannotSplitText,
-    NoneException,
-    get_or_raise_exception,
-    find_text_or_default
-)
+from bowi.initialize.converters.base import Converter
 from bowi.models import Document
 
 
@@ -32,14 +24,14 @@ class CmuConveter(Converter):
     item_pat: Pattern = field(init=False)
 
     def __post_init__(self):
-        self.item_pat = re.compile(r'^(?P<key>[a-zA-Z0-9]+): (?P<val>.+)$')
+        self.item_pat = re.compile(r'^(?P<key>[\-a-zA-Z0-9]+): (?P<val>.+)$')
 
     def _get_itemize(self,
                      line: str) -> Tuple[str, str]:
         """
         Read line which consists of an item name its value in the
         following format.
-        
+
         Item: value
 
         If line dosen't follow the format, this raises ItemNotFound
@@ -49,3 +41,44 @@ class CmuConveter(Converter):
             raise ItemNotFound()
         match: Match = _match
         return (match.group('key'), match.group('val'))
+
+    def _get_docid(self,
+                   fpath: Path) -> str:
+        docid: str = fpath.stem
+        return docid
+
+    def _get_tags(self,
+                  fpath: Path) -> List[str]:
+        """
+        Get categories (actually one category) from the path
+        """
+        cat: str = fpath.parent.name
+        return [cat, ]
+
+    def to_document(self,
+                    fpath: Path) -> Document:
+        docid: str = self._get_docid(fpath)
+        tags: List[str] = self._get_tags(fpath)
+        with open(fpath) as fin:
+            text: List[str] = fin.read().splitlines()
+        for i, line in enumerate(text):
+            if line == '':
+                continue
+            try:
+                key, val = self._get_itemize(line)
+            except ItemNotFound:
+                body: str = '\n'.join([s for s in text[i:] if s != ''])
+                break
+            if key == 'Subject':
+                title: str = val
+
+        try:
+            title
+        except NameError:
+            raise RuntimeError(f'Title is not found in {docid}')
+        try:
+            body
+        except NameError:
+            raise RuntimeError(f'Body is not found in {docid}')
+
+        return Document(docid=docid, tags=tags, text=body, title=title)
--- bowi/tests/initialize/converters/cmu_test.py
+++ bowi/tests/initialize/converters/cmu_test.py
@@ -1,5 +1,9 @@
 import pytest
 
+from pathlib import Path
+
+from bowi.models import Document
+
 from bowi.initialize.converters.cmu import ItemNotFound, CmuConveter
 
 
@@ -12,3 +16,58 @@ def test_get_itemize(converter):
     with pytest.raises(ItemNotFound):
         converter._get_itemize('')
     assert converter._get_itemize('key: value') == ('key', 'value')
+    line: str = (
+        'References: <1p9bseINNi6o@gap.caltech.edu>'
+        '<1pamva$b6j@fido.asd.sgi.com> <1pcq4pINNqp1@gap.caltech.edu> <11702@vice.ICO.TE'
+    )
+    key, val = converter._get_itemize(line)
+    assert key == 'References'
+    assert val[:5] == '<1p9b'
+    assert val[-5:] == 'CO.TE'
+
+
+def test_get_document(tmpdir, converter):
+    sample_dir: Path = Path(tmpdir) / 'rec.autos'
+    sample_dir.mkdir()
+    fpath: Path = sample_dir / '101557'
+    with open(fpath, 'w') as fout:
+        fout.write(
+            '''
+Newsgroups: rec.autos
+Path: cantaloupe.srv.cs.cmu.edu!rochester!udel!gatech!howland.reston.ans.net!newsserver.jvnc.net!siemens!siemens.com!tjo
+From: tjo@scr.siemens.com (Tom Ostrand)
+Subject: Radio for Toyota Tercel
+Message-ID: <tjo.734036386@siemens.com>
+Keywords: radio,Tercel,replacement
+Sender: news@scr.siemens.com (NeTnEwS)
+Nntp-Posting-Host: bugatti.siemens.com
+Organization: Siemens Corporate Research, Princeton (Plainsboro), NJ
+Date: Mon, 5 Apr 1993 18:59:46 GMT
+Lines: 19
+
+I'm looking for a replacement radio/tape player for a 1984
+Toyota Tercel.  Standard off-the-shelf unit is fine, but
+every place I've gone to (Service Merchandise, etc.) doesn't
+have my car in its model application book.  I want to just
+take out the old radio, and slide in the new, with minimal time
+spent hooking it up and adjusting the dashboard.
+
+If you have put in a new unit in a similar car, I'd like to hear
+what brand,  how easy it was to do the change, and any other
+relevant information.
+
+Please answer via E-mail.
+Thanks,  Tom Ostrand
+
+--
+Tom Ostrand			E-mail:  tjo@scr.siemens.com
+Siemens Corporate Research	Phone:   609-734-6569
+755 College Road East		FAX:     609-734-6565
+Princeton, NJ  08540-6668
+''')
+    doc: Document = converter.to_document(fpath)
+    assert doc.title == 'Radio for Toyota Tercel'
+    assert doc.tags == ['rec.autos', ]
+    assert doc.text[:5] == "I'm l"
+    assert doc.text[-5:] == "-6668"
+    assert doc.docid == '101557'
