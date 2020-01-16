9b2fa412dc2977eaf58cbc895edc91c98fe01b98
Wataru Hirota <nobuyoshi2426@gmail.com>
2020-01-16 11:06:42 +0000

    Register LDA and poisson

--- bowi/methods/run.py
+++ bowi/methods/run.py
@@ -12,7 +12,7 @@ from bowi.methods.common.methods import Method
 from bowi.methods.common.dumper import get_dump_dir
 
 # methods
-from bowi.methods.methods import keywords, bm25, topics, ttt
+from bowi.methods.methods import keywords, bm25, topics, ttt, lda, poisson
 from bowi.methods.methods.fuzzy import naive, rerank
 from bowi.initialize.cacher import embedding, pre_filtering
 
@@ -52,6 +52,10 @@ def get_method(method_name: str) -> Type[Method]:
         return topics.Topicrank
     elif method_name == 'ttt':
         return ttt.TTT
+    elif method_name == 'lda':
+        return lda.LDASearcher
+    elif method_name == 'poisson':
+        return poisson.Poisson
     else:
         raise KeyError(f'{method_name} is not found')
 
